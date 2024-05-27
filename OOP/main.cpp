577e4f0855a94fae163f8f6144a2a24d38ff9fe7
mega-mozg <abdulmunimikramov01@gmail.com>
2024-05-27 15:46:10 +0600

    Initial Commit

---
+++ OOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP
+//
+//  Created by Ikramov Abdul Munim on 27/5/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
